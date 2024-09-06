b953a9d5f7d5cfaac8f1fa567158ee15b7de8ce1
Yadickson Soto <yadickson@gmail.com>
2024-09-06 15:09:09 -0400

    Update unit test

--- test/number-mapper.spec.ts
+++ test/number-mapper.spec.ts
@@ -17,6 +17,21 @@ describe("NumberMapper tests", () => {
     expect(result).is.equal(defaultValue);
   });
 
+  it("should return default value when input is not numeric", () => {
+    const input = faker.string.uuid();
+    const result = mapper.mapper(input);
+    expect(result).to.be.not.eq(null);
+    expect(result).is.equal(defaultValue);
+  });
+
+  it("should return input default value when input is not numeric", () => {
+    const input = faker.string.uuid();
+    const inputDefault = faker.number.int();
+    const result = mapper.mapper(input, inputDefault);
+    expect(result).to.be.not.eq(null);
+    expect(result).is.equal(inputDefault);
+  });
+
   it("should return value when input is numeric", () => {
     const input = faker.number.int();
     const result = mapper.mapper(input);
